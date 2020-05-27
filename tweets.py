5c1cddd73884553146853f203c8167dc9ac5e631
Rajat Singhal <rajatsinghal564@gmail.com>
2020-05-27 19:04:41 +0530

    Location filtering tweets

--- tweets.py
+++ tweets.py
@@ -3,13 +3,14 @@ import json
 
 class MyStreamListener(tweepy.StreamListener):
 
-    # def on_status(self, status):
-    #     print(status.text)
-
     def on_data(self, data):
         # print(data.text)
         j = json.loads(data)
+        # print(j.keys())
         print(j['text'])
+        # print(j['geo'])
+        # print(j)
+        # print(j['location'])
 
 
 if __name__ == "__main__":
@@ -20,7 +21,13 @@ if __name__ == "__main__":
     auth = tweepy.OAuthHandler(tokens['api_key'], tokens['api_secret_key'])
     auth.set_access_token(tokens['access_token'], tokens['access_token_secret'])
 
-    myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth = auth, listener=myStreamListener)
+    with open('tweets_filter.json') as f:
+        tweets_filter = json.load(f)
 
-    myStream.filter(track=['python'])
+    languages = tweets_filter['languages']
+
+    for topics in tweets_filter['topics']:
+        myStreamListener = MyStreamListener()
+        myStream = tweepy.Stream(auth=auth, listener=myStreamListener)
+
+        myStream.filter(languages=languages, locations=topics['location'])
